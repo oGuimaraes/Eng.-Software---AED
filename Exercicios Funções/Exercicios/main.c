ea2b78d66c8dd4ccf1c3189adf2ec9abc1589a32
oGuimaraes <otaviovgsr@gmail.com>
2018-10-27 19:27:58 -0300

    Implementação do exercicio 7

--- Exercicios Funções/Exercicios/main.c
+++ Exercicios Funções/Exercicios/main.c
@@ -17,6 +17,8 @@ int main()
         printf("|---- |------------------------|\n");
         printf("|  1  |   Conjunto de Valores  |\n");
         printf("|  2  |    Checar Par e Impar  |\n");
+        printf("|  3  |      Divisivel por N   |\n");
+        printf("|  4  |        Mercadoria      |\n");
         printf("+-----+------------------------+\n");
         printf("\n");
         printf("Escolha um exercício: ");
@@ -30,6 +32,14 @@ int main()
 
         case 2:
             exercicio4();
+            break;
+
+        case 3:
+            exercicio5();
+            break;
+
+        case 4:
+            exercicio7();
             break;
 
         case 0:
@@ -50,19 +60,22 @@ int main()
 }
 
 
-int maiorquezero(){
+int maiorquezero()
+{
     int n = 0;
     printf("Escolha um número maior que zero: ");
     scanf("%d", & n);
 
-    while(n < 0){
+    while(n < 0)
+    {
         printf("Escolha um número maior que 0: ");
         scanf("%d", & n);
     }
     return n;
 }
 
-int hipotenusa(int cat1,int cat2){
+int hipotenusa(int cat1,int cat2)
+{
     int h = 0;
 
     cat1 = cat1 * cat1;
@@ -74,13 +87,15 @@ int hipotenusa(int cat1,int cat2){
 
 }
 
-int exercicio3(){
+int exercicio3()
+{
     int x, a, b;
 
     a = maiorquezero();
     b = maiorquezero();
 
-    while (a >= 0 && b >= 0){
+    while (a >= 0 && b >= 0)
+    {
         x = hipotenusa(a, b);
 
         printf("A hipotenusa é: %d \n", x);
@@ -91,21 +106,26 @@ int exercicio3(){
     }
 
 }
-int checkParImpar(int n){
+int checkParImpar(int n)
+{
     bool t = true;
-    if (n % 2 == 1 ){
+    if (n % 2 == 1 )
+    {
         t = false;
     }
     return t;
 }
 
-int exercicio4(){
+int exercicio4()
+{
     int num, cont = 0;
     num = maiorquezero();
 
 
-    while (num != 0){
-        if (checkParImpar(num)){
+    while (num != 0)
+    {
+        if (checkParImpar(num))
+        {
             printf("Par:%d \n", num);
             cont = cont + 1;
         }
@@ -113,3 +133,80 @@ int exercicio4(){
     }
     printf("Total de números pares: %d \n", cont);
 }
+
+int nDivisivelN(int num, int div)
+{
+
+    if (num % div == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int exercicio5()
+{
+    int num1, num2;
+    int checkFunction;
+
+    num1 = maiorquezero();
+    num2 = maiorquezero();
+
+    while (num1 > 0 && num2 > 0)
+    {
+        checkFunction = nDivisivelN(num1, num2);
+        if (checkFunction == 1)
+        {
+            printf("%d é divisível por %d \n \n", num1, num2);
+        }
+        num1 = maiorquezero();
+        num2 = maiorquezero();
+    }
+}
+
+
+float codigoPreco(preco)
+
+{
+    switch(preco)
+    {
+    case 100:
+        return 1.20;
+    break;
+
+    case 101:
+        return 1.50;
+    break;
+
+    case 102:
+        return 2.50;
+    break;
+
+    case 103:
+        return 2.75;
+    break;
+
+    default:
+        printf("Valor inválido");
+    }
+}
+
+int exercicio7(){
+    int num, quantidade;
+    float total, preco;
+
+    printf("Digite um código: ");
+    scanf("%d", & num);
+
+    printf("Digite a quantidade: ");
+    scanf("%d", & quantidade);
+
+    total = codigoPreco(num) * quantidade;
+
+    printf("O total é %.2f \n", total);
+
+}
+
