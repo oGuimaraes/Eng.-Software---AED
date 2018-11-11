94f60d568fc89f788d6c4590f84e6ed1c51fb207
oGuimaraes <otaviovgsr@gmail.com>
2018-11-11 03:09:14 -0200

    Exercicio 2 - Arranjos

--- Exercicios Arranjo/main.c
+++ Exercicios Arranjo/main.c
@@ -27,6 +27,10 @@ int main()
             exercicio1();
             break;
 
+        case 2:
+            exercicio2();
+            break;
+
         case 0:
             system("pause");
             break;
@@ -44,13 +48,14 @@ int main()
     return 0;
 }
 
-void leVetor(int *, int);
+void leVetor(int *, int,char *);
 void imprimeVetor(int *, int, char *);
 int fatorial(int);
+int confereIgualdade(int *, int, int);
 
-void leVetor(int *vetor, int tamVetor){
+void leVetor(int *vetor, int tamVetor, char *nomeVetor){
     for (int i = 0; i < tamVetor; i++){
-        printf("Digite o %dº termo do vetor: ", i+1);
+        printf("Digite o %dº termo do vetor %s: ", i+1, nomeVetor);
         scanf("%d", &vetor[i]);
     }
 }
@@ -73,14 +78,48 @@ int fatorial(int numero)
     return fat;
 }
 
+int confereIgualdade(int *v, int tamV, int elemento){
+    int i = 0, existe = 0;
+
+    while (!existe && i < tamV){
+        if (v[i] == elemento){
+            existe = 1;
+        }
+        i++;
+    }
+    return existe;
+}
+
 int exercicio1(){
-    setlocale(LC_ALL, "portuguese");
     int A[5], B[5], i;
-    leVetor(A, 5);
+    leVetor(A, 5, "A");
     for (i = 0; i < 5 ; i++){
         B[i] = fatorial(A[i]);
     }
     imprimeVetor(B,5,"B");
+    return 0;
+}
+
+int exercicio2(){
+   int A[5], B[5], C[10], i, tamC = 0;
+   leVetor(A, 5, "A");
+   leVetor(B, 5, "B");
+
+   for (i = 0; i < 5; i++){
+        if(!confereIgualdade(C, tamC, A[i])){
+            C[tamC] = A[i];
+            tamC++;
+        }
+   }
+
+    for (i = 0; i < 5; i++){
+        if(!confereIgualdade(C, tamC, B[i])){
+            C[tamC] = B[i];
+            tamC++;
+        }
+   }
+   imprimeVetor(C,tamC, "C");
+   printf("\n");
     return 0;
 }
 
